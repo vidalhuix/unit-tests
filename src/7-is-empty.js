9a329ef1a1fb7735e3baa6b4fd27ae34a87a3a07
Your Name <you@example.com>
2024-04-30 14:49:04 +0200

    unit tests complete

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,13 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
+  const type = typeof stringArrayOrObject;
 
   if (type === 'string') {
-    return stringArrayOrObject === ''
+    return stringArrayOrObject === '';
+  } else if (Array.isArray(stringArrayOrObject)) {
+    return stringArrayOrObject.length === 0;
+  } else if (type === 'object') {
+    return Object.keys(stringArrayOrObject).length === 0;
   }
 
-  return false
-}
+  return false;
+};
--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,7 @@
 export const hashtags = (text) => {
-  return text
-}
+  // Split the text into an array of words
+  const words = text.split(' ');
+
+  // Filter out words that contain hashtags
+  return words.filter(word => word.includes('#'));
+};
